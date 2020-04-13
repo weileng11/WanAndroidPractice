6f2e1bb7b6ebf370a30376c8d1786682afc40a97
litao <litao143>
2020-04-13 15:46:34 +0800

    kt 加入拦截器日志打印

--- ktglobalscopelx/src/main/java/com/bruce/ktglobalscopelx/Main2Activity.kt
+++ ktglobalscopelx/src/main/java/com/bruce/ktglobalscopelx/Main2Activity.kt
@@ -1,11 +1,7 @@
 package com.bruce.ktglobalscopelx
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.bruce.ktglobalscopelx.R
 import com.orhanobut.logger.Logger
-import kotlinx.coroutines.*
 
 class Main2Activity : BaseCoroutineScopeActivity() {
 
--- wanandroidsx/src/main/java/com/bruce/sx/base/WanAndroidApplication.kt
+++ wanandroidsx/src/main/java/com/bruce/sx/base/WanAndroidApplication.kt
@@ -15,6 +15,9 @@ import com.scwang.smartrefresh.layout.api.RefreshFooter
 import com.scwang.smartrefresh.layout.api.RefreshHeader
 import com.scwang.smartrefresh.layout.api.RefreshLayout
 import com.scwang.smartrefresh.layout.footer.BallPulseFooter
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import com.bruce.sx.BuildConfig
 
 
 /**
@@ -28,7 +31,14 @@ class WanAndroidApplication : Application() {
         super.onCreate()
         baseApplication = this
         context = applicationContext
+        //参考:https://blog.csdn.net/asialyf/article/details/79067374
+        //https://www.jianshu.com/p/5302f6b11eaa
         Logger.addLogAdapter(AndroidLogAdapter())
+        Logger.addLogAdapter(object : AndroidLogAdapter() {
+            override fun isLoggable(priority: Int, tag: String?): Boolean {
+                return BuildConfig.DEBUG
+            }
+        })
     }
 
     init {
@@ -44,16 +54,16 @@ class WanAndroidApplication : Application() {
         init {
             //设置全局的Header构建器
             SmartRefreshLayout.setDefaultRefreshHeaderCreator { context, layout ->
-                layout.setPrimaryColorsId(R.color.colorPrimary, android.R.color.white)//全局设置主题颜色
+                layout.setPrimaryColorsId(com.bruce.sx.R.color.colorPrimary, android.R.color.white)//全局设置主题颜色
                 MaterialHeader(context)//.setTimeFormat(new DynamicTimeFormat("更新于 %s"));//指定为经典Header，默认是 贝塞尔雷达Header
             }
             //设置全局的Footer构建器
             SmartRefreshLayout.setDefaultRefreshFooterCreator { context, layout ->
-                layout.setPrimaryColorsId(R.color.colorPrimary, android.R.color.white)//全局设置主题颜色
+                layout.setPrimaryColorsId(com.bruce.sx.R.color.colorPrimary, android.R.color.white)//全局设置主题颜色
                 //指定为经典Footer，默认是 BallPulseFooter
                 val footer = BallPulseFooter(context)
-                footer.setAnimatingColor(ColorUtils.parseColor(R.color.theme))
-                footer.setBackgroundColor(ColorUtils.parseColor(R.color.white))
+                footer.setAnimatingColor(ColorUtils.parseColor(com.bruce.sx.R.color.theme))
+                footer.setBackgroundColor(ColorUtils.parseColor(com.bruce.sx.R.color.white))
                 footer
             }
         }
